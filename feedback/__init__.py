6ebe1c637255a5ab40421db04cdc8dc332b4cd26
Stas Rudakou <dev@nott.cc>
2011-10-16 16:10:27 +0300

    Add setup.py script

---
+++ feedback/__init__.py
@@ -0,0 +1,6 @@
+"""
+django-pyres-feedback is an application for
+feedback submission via Pyres in Django. 
+"""
+version = (0, 0, 1)
+__version__ = '.'.join(map(str, version))
---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+""" Setup file for easy installation """
+from setuptools import setup
+
+
+version = __import__('feedback').__version__
+
+LONG_DESCRIPTION = """
+django-pyres-feedback is an application for
+feedback submission via Pyres in Django. 
+"""
+
+setup(name='django-pyres-feedback',
+      version=version,
+      author='Stas Rudakou',
+      author_email='dev@nott.cc',
+      description='Feedback submission in Django via Pyres.',
+      license='BSD',
+      keywords='django, pyres, feedback, application',
+      url='https://github.com/nott/django-pyres-feedback',
+      packages=['feedback',],
+      long_description=LONG_DESCRIPTION,
+      install_requires=['django>=1.2.0',
+                        'pyres>=1.0',],
+      classifiers=['Framework :: Django',
+                   'Development Status :: 4 - Beta',
+                   'Topic :: Internet',
+                   'License :: OSI Approved :: BSD License',
+                   'Intended Audience :: Developers',
+                   'Environment :: Web Environment',
+                   'Programming Language :: Python :: 2.6',
+                   'Programming Language :: Python :: 2.7'])
